42f2ec4110faa6d45f8c8457e2126d9dcb11f157
Anand <anandkumarlnctu02@gmail.com>
2025-07-18 23:04:35 +0530

    Add backend folder: hirewirebe

---
+++ hirewirebe/src/main/java/com/project/HireWire/HireWireApplication.java
@@ -0,0 +1,13 @@
+package com.project.HireWire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireWireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireWireApplication.class, args);
+	}
+
+}
---
+++ hirewirebe/src/test/java/com/project/HireWire/HireWireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.HireWire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireWireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
